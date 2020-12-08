0777e1dfd448fd4bd6a124f9806e8c22ef768e94
christopher0404 <ctc2096@gmail.com>
2020-12-08 21:23:10 +0800

    day-07

---
+++ day-07/partOne.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8');
+
+function solvePartOne(input) {
+  const rules = input.split('\n').reduce((rules, line) => {
+    const [, color, otherColors] = /(\w+ \w+) bags contain (.*)\./.exec(line);
+
+    const compatibleWith = otherColors !== 'no other bags'
+      ? otherColors.split(', ').map((other) => /(\w+ \w+) bags?/.exec(other)[1])
+      : [];
+
+    rules[color] = new Set();
+
+    compatibleWith.forEach(otherColor => rules[color].add(otherColor));
+
+    return rules;
+  }, {});
+
+  const expand = bag => {
+    const colors = [...rules[bag].values()];
+
+    for (const color of rules[bag].values()) {
+      colors.push(...expand(color));
+    }
+
+    return colors;
+  };
+
+  return Object.keys(rules).filter(key => expand(key).includes('shiny gold')).length;
+}
+
+console.log(solvePartOne(input));
+
+module.exports = solvePartOne;
---
+++ day-07/partTwo.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('input.txt', 'utf8');
+
+function solvePartTwo(input) {
+  const rules = input.split('\n').reduce((rules, line) => {
+    const [, color, otherColors] = /(\w+ \w+) bags contain (.*)\./.exec(line);
+
+    const compatibleWith = otherColors !== 'no other bags'
+      ? otherColors.split(', ').map((other) => {
+        const [, units, color] = /(\d+) (\w+ \w+) bags?/.exec(other);
+
+        return { units: parseInt(units), color };
+      })
+      : [];
+
+    rules.set(color, []);
+
+    compatibleWith.forEach(otherColor => rules.get(color).push(otherColor));
+
+    return rules;
+  }, new Map());
+
+  const traverse = bag => {
+    let total = 0;
+
+    for (const { color, units } of rules.get(bag)) {
+      total += units + units * traverse(color);
+    }
+
+    return total;
+  };
+
+  return traverse('shiny gold');
+}
+
+console.log(solvePartTwo(input));
+
+module.exports = solvePartTwo;
